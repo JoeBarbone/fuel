5e979159b9c87c8099b4d5de6397e0323a1c6bab
Joseph Barbone <JoeBarbone@aol.com>
2023-10-24 08:43:03 -0400

    added delete icon/action to displayed records

--- src/pages/Footer.js
+++ src/pages/Footer.js
@@ -6,7 +6,8 @@ const Footer = () => {
     <div className='footer'>
         <p>Created and designed by Midnight Angel Web Development</p>
         <p>Icons by <a href='http://flaticon.com' target='_new'>FlatIcon</a></p>
-        <p>Logo created with <a href='http://brandcrowd.com' target='_new'>Brand Crowd</a></p>
+        <p><a href="https://www.flaticon.com/free-icons/delete" title="delete icons">Delete icons created by IYAHICON - Flaticon</a></p>
+        <p>Logo created by <a href='http://brandcrowd.com' target='_new'>Brand Crowd</a></p>
     </div>
   )
 }
