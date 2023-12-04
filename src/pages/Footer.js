dd43a362b72d9301e3fb3bb5329d13a9c7ba44cb
Joseph Barbone <JoeBarbone@aol.com>
2023-12-04 16:23:47 -0500

    changed footer to BITS

--- src/pages/Footer.js
+++ src/pages/Footer.js
@@ -4,7 +4,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <div className='footer'>
-        <p>Created and designed by Midnight Angel Web Development</p>
+        <p>&copy;2023 Barbone <span style={{color: "red"}}>IT</span> Solutions, LLC </p>
         <p>Icons by <a href='http://flaticon.com' target='_new'>FlatIcon</a></p>
         <p><a href="https://www.flaticon.com/free-icons/delete" title="delete icons">Delete icons created by IYAHICON - Flaticon</a></p>
         <p>Logo created by <a href='http://brandcrowd.com' target='_new'>Brand Crowd</a></p>
