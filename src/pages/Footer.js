c2a4821221537f30426e7d1d00d3094d9f3c1ed3
Joseph Barbone <JoeBarbone@aol.com>
2024-03-09 15:10:17 -0500

    Test version by changing IT color in footer

--- src/pages/Footer.js
+++ src/pages/Footer.js
@@ -4,7 +4,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <div className='footer'>
-        <p>Created and designed by Barbone IT Solutions, LLC</p>
+        <p>Created and designed by Barbone <span className='it'>IT</span> Solutions, LLC</p>
         <p>Icons by <a href='http://flaticon.com' target='_new'>FlatIcon</a></p>
         <p>Logo created with <a href='http://brandcrowd.com' target='_new'>Brand Crowd</a></p>
     </div>
