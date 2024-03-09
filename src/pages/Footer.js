dfcaa2520803b1d889ce2c69018fc0dfad6ab899
Joseph Barbone <JoeBarbone@aol.com>
2024-03-09 15:18:33 -0500

    Fixed Footer.js merge

--- src/pages/Footer.js
+++ src/pages/Footer.js
@@ -4,11 +4,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <div className='footer'>
-<<<<<<< HEAD
         <p>Created and designed by Barbone <span className='it'>IT</span> Solutions, LLC</p>
-=======
-        <p>&copy;2023 Barbone <span style={{color: "red"}}>IT</span> Solutions, LLC </p>
->>>>>>> dd43a362b72d9301e3fb3bb5329d13a9c7ba44cb
         <p>Icons by <a href='http://flaticon.com' target='_new'>FlatIcon</a></p>
         <p><a href="https://www.flaticon.com/free-icons/delete" title="delete icons">Delete icons created by IYAHICON - Flaticon</a></p>
         <p>Logo created by <a href='http://brandcrowd.com' target='_new'>Brand Crowd</a></p>
