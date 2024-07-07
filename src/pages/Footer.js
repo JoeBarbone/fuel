f766392e64241703ab0f74e137414fafc4ad416e
Joseph Barbone <JoeBarbone@aol.com>
2024-07-07 17:52:37 -0400

    removed delete button, this may have already been synced

--- src/pages/Footer.js
+++ src/pages/Footer.js
@@ -6,8 +6,8 @@ const Footer = () => {
     <div className='footer'>
         <p>Created and designed by Barbone <span className='it'>IT</span> Solutions, LLC</p>
         <p>Icons by <a href='http://flaticon.com' target='_new'>FlatIcon</a></p>
-        <p><a href="https://www.flaticon.com/free-icons/delete" title="delete icons">Delete icons created by IYAHICON - Flaticon</a></p>
-        <p>Logo created by <a href='http://brandcrowd.com' target='_new'>Brand Crowd</a></p>
+        {/* <p><a href="https://www.flaticon.com/free-icons/delete" title="delete icons">Delete icons created by IYAHICON - Flaticon</a></p> */}
+        <p>Logo by <a href='http://brandcrowd.com' target='_new'>Brand Crowd</a></p>
     </div>
   )
 }
